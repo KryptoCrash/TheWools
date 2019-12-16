d57649154de378b2ae04959cd4a4eaf2d466f75f
KryptoCrash <kryptocrashmh@gmail.com>
2019-12-15 21:47:36 -0500

    Creating Plugin Template

---
+++ src/ml/thewools/thewools/TheWools.java
@@ -0,0 +1,16 @@
+package ml.thewools.thewools;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TheWools extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
