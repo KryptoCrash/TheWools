8fce9d4f29085db76faca1bc2a997018ef20a136
00100000 <53249541+00100000spacebar@users.noreply.github.com>
2019-12-28 17:08:12 -0500

    Created Leggings
    I'll add my class files later I guess

---
+++ src/ml/thewools/thewools/Items/BlazeKit/BlazeLeggings.java
@@ -0,0 +1,19 @@
+package ml.thewools.thewools.Items.BlazeKit;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+import ml.thewools.thewools.Items.Armor;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class BlazeLeggings extends Armor implements Listener {
+    public ItemStack item;
+    public BlazeLeggings() {
+        this.item = new ItemStack(Material.LEATHER_LEGGINGS);
+        ItemMeta meta = item.getItemMeta();
+        meta.setLore(this.generateLore(ChatColor.GREEN + "Blaze Leggings", 30, 0));
+        meta.setDisplayName(ChatColor.GREEN + "Blaze Leggings");
+        item.setItemMeta(meta);
+    }
+}
