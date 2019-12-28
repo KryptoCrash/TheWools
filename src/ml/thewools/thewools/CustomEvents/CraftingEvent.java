312036522806cec2a7594207bce4638a00405e86
00100000 <53249541+00100000spacebar@users.noreply.github.com>
2019-12-28 17:55:31 -0500

    Added Support for Modified Crafts
    Sometimes you feel like crafting boots and helmets at the bottom of the crafting table, so now you can!

--- src/ml/thewools/thewools/CustomEvents/CraftingEvent.java
+++ src/ml/thewools/thewools/CustomEvents/CraftingEvent.java
@@ -47,6 +47,19 @@ public class CraftingEvent implements Listener {
             put(3, new ItemStack(Material.BLAZE_ROD));
             put(5, new ItemStack(Material.BLAZE_ROD));
         }});
+        checkCraft(new BlazeHelmet().item, e.getInventory(), new HashMap<Integer, ItemStack>() {{
+            put(3, new ItemStack(Material.BLAZE_ROD));
+            put(4, new ItemStack(Material.BLAZE_ROD));
+            put(5, new ItemStack(Material.BLAZE_ROD));
+            put(6, new ItemStack(Material.BLAZE_ROD));
+            put(8, new ItemStack(Material.BLAZE_ROD));
+        }});
+        checkCraft(new BlazeBoots().item, e.getInventory(), new HashMap<Integer, ItemStack>() {{
+            put(3, new ItemStack(Material.BLAZE_ROD));
+            put(5, new ItemStack(Material.BLAZE_ROD));
+            put(6, new ItemStack(Material.BLAZE_ROD));
+            put(8, new ItemStack(Material.BLAZE_ROD));
+        }});
     }
     public void checkCraft(ItemStack result, CraftingInventory inv, HashMap<Integer, ItemStack> ingredients) {
         ItemStack[] matrix = inv.getMatrix();
