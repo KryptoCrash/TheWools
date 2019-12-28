ad4b21a6ff9b8fc7dde5b905573b26464909193e
00100000 <53249541+00100000spacebar@users.noreply.github.com>
2019-12-28 17:09:03 -0500

    Created Blaze Boots
    Completed the Blaze Set.

---
+++ src/ml/thewools/thewools/Items/BlazeKit/BlazeBoots.java
@@ -0,0 +1,19 @@
+package ml.thewools.thewools.Items.BlazeKit;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+import ml.thewools.thewools.Items.Armor;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class BlazeBoots extends Armor implements Listener {
+    public ItemStack item;
+    public BlazeBoots() {
+        this.item = new ItemStack(Material.LEATHER_BOOTS);
+        ItemMeta meta = item.getItemMeta();
+        meta.setLore(this.generateLore(ChatColor.GREEN + "Blaze Boots", 30, 0));
+        meta.setDisplayName(ChatColor.GREEN + "Blaze Boots");
+        item.setItemMeta(meta);
+    }
+}
