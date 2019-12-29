96ab430b708bd7f6cc312bf4357ffd7b05627e31
00100000 <53249541+00100000spacebar@users.noreply.github.com>
2019-12-29 16:17:04 -0500

    Create MagmarChestplate.java

---
+++ src/ml/thewools/thewools/Items/MagmarKit/MagmarChestplate.java
@@ -0,0 +1,19 @@
+package ml.thewools.thewools.Items.MagmarKit;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+import ml.thewools.thewools.Items.Armor;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class MagmarChestplate extends Armor implements Listener {
+    public ItemStack item;
+    public MagmarChestplate() {
+        this.item = new ItemStack(Material.LEATHER_CHESTPLATE);
+        ItemMeta meta = item.getItemMeta();
+        meta.setLore(this.generateLore(ChatColor.RED + "Magmar Chestplate", 30, 0));
+        meta.setDisplayName(ChatColor.RED + "Magmar Chestplate");
+        item.setItemMeta(meta);
+    }
+}
