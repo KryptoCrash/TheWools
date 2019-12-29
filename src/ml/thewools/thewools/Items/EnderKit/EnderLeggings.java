199ff1a72a48077d5e077949bdc831b5febe143e
00100000 <53249541+00100000spacebar@users.noreply.github.com>
2019-12-29 16:28:19 -0500

    Create EnderLeggings.java

---
+++ src/ml/thewools/thewools/Items/EnderKit/EnderLeggings.java
@@ -0,0 +1,19 @@
+package ml.thewools.thewools.Items.EnderKit;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+import ml.thewools.thewools.Items.Armor;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class EnderLeggings extends Armor implements Listener {
+    public ItemStack item;
+    public EnderLeggings() {
+        this.item = new ItemStack(Material.LEATHER_LEGGINGS);
+        ItemMeta meta = item.getItemMeta();
+        meta.setLore(this.generateLore(ChatColor.PURPLE + "Ender Leggings", 30, 0));
+        meta.setDisplayName(ChatColor.PURPLE + "Ender Leggings");
+        item.setItemMeta(meta);
+    }
+}
