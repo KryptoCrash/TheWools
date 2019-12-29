504b39407df40a89ecc20a5ec98ad499f993bc14
00100000 <53249541+00100000spacebar@users.noreply.github.com>
2019-12-29 09:27:05 -0500

    Added Skull Recipes for Wither Armor
    Can work with any skulls because I can't find the IDs of wither skeleton skulls.

--- src/ml/thewools/thewools/CustomEvents/CraftingEvent.java
+++ src/ml/thewools/thewools/CustomEvents/CraftingEvent.java
@@ -4,6 +4,10 @@ import ml.thewools.thewools.Items.BlazeKit.BlazeHelmet;
 import ml.thewools.thewools.Items.BlazeKit.BlazeChestplate;
 import ml.thewools.thewools.Items.BlazeKit.BlazeLeggings;
 import ml.thewools.thewools.Items.BlazeKit.BlazeBoots;
+import ml.thewools.thewools.Items.WitherKit.WitherHelmet;
+import ml.thewools.thewools.Items.WitherKit.WitherChestplate;
+import ml.thewools.thewools.Items.WitherKit.WitherLeggings;
+import ml.thewools.thewools.Items.WitherKit.WitherBoots;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -62,6 +66,51 @@ public class CraftingEvent implements Listener {
             put(5, new ItemStack(Material.BLAZE_ROD));
             put(6, new ItemStack(Material.BLAZE_ROD));
             put(8, new ItemStack(Material.BLAZE_ROD));
+        }});
+        checkCraft(new WitherHelmet().item, e.getInventory(), new HashMap<Integer, ItemStack>() {{
+            put(0, new ItemStack(Material.SKULL));
+            put(1, new ItemStack(Material.SKULL));
+            put(2, new ItemStack(Material.SKULL));
+            put(3, new ItemStack(Material.SKULL));
+            put(5, new ItemStack(Material.SKULL));
+        }});
+        checkCraft(new WitherChestplate().item, e.getInventory(), new HashMap<Integer, ItemStack>() {{
+            put(0, new ItemStack(Material.SKULL));
+            put(2, new ItemStack(Material.SKULL));
+            put(3, new ItemStack(Material.SKULL));
+            put(4, new ItemStack(Material.SKULL));
+            put(5, new ItemStack(Material.SKULL));
+            put(6, new ItemStack(Material.SKULL));
+            put(7, new ItemStack(Material.SKULL));
+            put(8, new ItemStack(Material.SKULL));
+        }});
+        checkCraft(new WitherLeggings().item, e.getInventory(), new HashMap<Integer, ItemStack>() {{
+            put(0, new ItemStack(Material.SKULL));
+            put(1, new ItemStack(Material.SKULL));
+            put(2, new ItemStack(Material.SKULL));
+            put(3, new ItemStack(Material.SKULL));
+            put(5, new ItemStack(Material.SKULL));
+            put(6, new ItemStack(Material.SKULL));
+            put(8, new ItemStack(Material.SKULL));
+        }});
+        checkCraft(new WitherBoots().item, e.getInventory(), new HashMap<Integer, ItemStack>() {{
+            put(0, new ItemStack(Material.SKULL));
+            put(2, new ItemStack(Material.SKULL));
+            put(3, new ItemStack(Material.SKULL));
+            put(5, new ItemStack(Material.SKULL));
+        }});
+        checkCraft(new WitherHelmet().item, e.getInventory(), new HashMap<Integer, ItemStack>() {{
+            put(3, new ItemStack(Material.SKULL));
+            put(4, new ItemStack(Material.SKULL));
+            put(5, new ItemStack(Material.SKULL));
+            put(6, new ItemStack(Material.SKULL));
+            put(8, new ItemStack(Material.SKULL));
+        }});
+        checkCraft(new WitherBoots().item, e.getInventory(), new HashMap<Integer, ItemStack>() {{
+            put(3, new ItemStack(Material.SKULL));
+            put(5, new ItemStack(Material.SKULL));
+            put(6, new ItemStack(Material.SKULL));
+            put(8, new ItemStack(Material.SKULL));
         }});
     }
     public void checkCraft(ItemStack result, CraftingInventory inv, HashMap<Integer, ItemStack> ingredients) {
