e823dd655ece67b56f40cde4a8de44bf010a546d
00100000 <53249541+00100000spacebar@users.noreply.github.com>
2019-12-29 09:12:02 -0500

    Create WitherChestplate.java

---
+++ src/ml/thewools/thewools/Items/WitherKit/WitherChestplate.java
@@ -0,0 +1,19 @@
+package ml.thewools.thewools.Items.WitherKit;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+import ml.thewools.thewools.Items.Armor;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class WitherChestplate extends Armor implements Listener {
+    public ItemStack item;
+    public WitherChestplate() {
+        this.item = new ItemStack(Material.LEATHER_CHESTPLATE);
+        ItemMeta meta = item.getItemMeta();
+        meta.setLore(this.generateLore(ChatColor.BLACK + "Wither Chestplate", 30, 0));
+        meta.setDisplayName(ChatColor.BLACK + "Wither Chestplate");
+        item.setItemMeta(meta);
+    }
+}
