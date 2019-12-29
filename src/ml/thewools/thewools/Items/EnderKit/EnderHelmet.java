3eff383acb68fa55755958730ed5912e93aee0d1
00100000 <53249541+00100000spacebar@users.noreply.github.com>
2019-12-29 16:26:43 -0500

    Update EnderHelmet.java

--- src/ml/thewools/thewools/Items/EnderKit/EnderHelmet.java
+++ src/ml/thewools/thewools/Items/EnderKit/EnderHelmet.java
@@ -1,1 +1,19 @@
+package ml.thewools.thewools.Items.EnderKit;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+import ml.thewools.thewools.Items.Armor;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class EnderHelmet extends Armor implements Listener {
+    public ItemStack item;
+    public EnderHelmet() {
+        this.item = new ItemStack(Material.LEATHER_HELMET);
+        ItemMeta meta = item.getItemMeta();
+        meta.setLore(this.generateLore(ChatColor.PURPLE + "Ender Helmet", 30, 0));
+        meta.setDisplayName(ChatColor.PURPLE + "Ender Helmet");
+        item.setItemMeta(meta);
+    }
+}
