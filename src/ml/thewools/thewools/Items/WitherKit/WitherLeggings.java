511cf4985ca69305096b48349e9435b47996f784
00100000 <53249541+00100000spacebar@users.noreply.github.com>
2019-12-29 09:12:49 -0500

    Create WitherLeggings.java

---
+++ src/ml/thewools/thewools/Items/WitherKit/WitherLeggings.java
@@ -0,0 +1,19 @@
+package ml.thewools.thewools.Items.WitherKit;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+import ml.thewools.thewools.Items.Armor;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class WitherLeggings extends Armor implements Listener {
+    public ItemStack item;
+    public WitherLeggings() {
+        this.item = new ItemStack(Material.LEATHER_LEGGINGS);
+        ItemMeta meta = item.getItemMeta();
+        meta.setLore(this.generateLore(ChatColor.BLACK + "Wither Leggings", 30, 0));
+        meta.setDisplayName(ChatColor.BLACK + "Wither Leggings");
+        item.setItemMeta(meta);
+    }
+}
