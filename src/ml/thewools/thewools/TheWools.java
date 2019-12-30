95a54b6cebed362920023f44dbd24fccd904cb60
KryptoCrash <kryptocrashmh@gmail.com>
2019-12-30 12:47:56 -0500

    Added Damage Event to Main Class

--- src/ml/thewools/thewools/TheWools.java
+++ src/ml/thewools/thewools/TheWools.java
@@ -1,12 +1,14 @@
 package ml.thewools.thewools;
 
 import ml.thewools.thewools.CustomEvents.CraftingEvent;
+import ml.thewools.thewools.CustomEvents.DamageEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TheWools extends JavaPlugin {
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(new CraftingEvent(), this);
+        getServer().getPluginManager().registerEvents(new DamageEvent(), this);
     }
 
     @Override
