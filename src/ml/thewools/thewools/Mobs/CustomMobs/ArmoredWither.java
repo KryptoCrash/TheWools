87103aebc2118ceaf6954a410d19e2afb67a959a
Unum <zyxokunum@outlook.com>
2020-01-10 21:25:07 -0500

    This is why you don't commit to master without an IDE. @PatheticMustan

--- src/ml/thewools/thewools/Mobs/CustomMobs/ArmoredWither.java
+++ src/ml/thewools/thewools/Mobs/CustomMobs/ArmoredWither.java
@@ -1,13 +1,15 @@
 package ml.thewools.thewools.Mobs.CustomMobs;
 
 import ml.thewools.thewools.Items.*;
+import ml.thewools.thewools.Mobs.MobBuilder;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.entity.*;
-import org.bukkit.event.Listener;
 
 public class ArmoredWither {
     private WitherSkeleton entity;
 
-    public ArmoredWither() {
-        entity = new MobBuilder().setArmor(ArmorKitAll.WITHER).setType(EntityType.WITHER_SKELETON);
+    public ArmoredWither(World world, Location location) {
+        entity = (WitherSkeleton) new MobBuilder().setArmor(ArmorKitAll.WITHER).setType(EntityType.WITHER_SKELETON).spawn(world, location);
     }
 }
